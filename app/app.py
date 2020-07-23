910a3d7844ab3d106cf50bfbb889acf247e5528b
Wayne <w.p.lam107@gmail.com>
2020-07-23 16:59:35 -0400

    close to mvp app

--- app/app.py
+++ app/app.py
@@ -3,24 +3,119 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 import plotly.express as px
+import plotly.graph_objects as go
 
 import pandas as pd
 import pickle
 
 from app_functions import *
 
-# Dash tutorial CSS
+# Dash tutorial CSS, major thanks to Chris Parmer of Plotly/Dash for his answers on the community forums
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-with open('./data/mul_df.p', 'rb') as f:
-    df = pickle.load(f)
+# Load all data
+# List of all courts
+with open('./data/courts.p', 'rb') as f:
+    courts = pickle.load(f)
 
+# DataFrame of each court projected onto 2 principal components
+with open('./data/pca_df.p', 'rb') as f:
+    pca_df = pickle.load(f)
 
-app.layout = html.Div([
-    dcc.Graph(id='graph-with-slider'),
+# List of similarity matrices of all courts
+with open('./data/sim_mats.p', 'rb') as f:
+    sim_mats = pickle.load(f)
+
+# Get justice options
+justice_options = ['All'] + get_justices(courts)
+
+markdown_1 = '''Choose a Supreme Court Justice who served between 1999-2019.
+The courts the justice has served on will appear in the dropdown menu on the right.
+Below is a 2-component scatter plot for the courts the selected justice has served on.
+More on principal component analysis (PCA) at [Wikipedia](https://en.wikipedia.org/wiki/Principal_component_analysis).
+'''
+
+markdown_2 = '''Choose a Supreme Court by justice composition of the court.
+The selectable courts are the court compositions that the selected justice has served on.
+The heatmaps below are cosine similarity heatmaps (0 = no similarity, 1 = exactly the same) for the selected court.
+More on cosine similarity at [Wikipedia](https://en.wikipedia.org/wiki/Cosine_similarity).
+'''
+
+app.layout = html.Div(children=[
+    html.Div(
+        className='banner',
+        children=[
+            html.H1(
+                id='banner-title',
+                children=[
+                    html.A(
+                        'SCOTUS Dashboard',
+                        href='https://github.com/wplam107/scotus_rulings',
+                    )
+                ],
+            )
+    ]),
+
+    html.Div(children=[
+        html.Div(children=[dcc.Markdown(children=markdown_1)], className='six columns'),
+        html.Div(children=[dcc.Markdown(children=markdown_2)], className='six columns'),
+    ], className='row'
+    ),
+
+    html.Hr(),
+
+    html.Div([
+        html.Div(children='Select Justice'),
+        dcc.Dropdown(
+            id='justice-select',
+            options=[{'label': j, 'value': j} for j in justice_options],
+            value = 'All',
+            clearable=False,
+        ),
+        dcc.Graph(id='pca-graph'),
+    ],
+    style={'width': '49%', 'float': 'left', 'display': 'inline-block'}),
+
+    html.Div([
+        html.Div(children='Select Court'),
+        dcc.Dropdown(
+            id='court-select',
+            value = 0,
+            clearable=False,
+        ),
+        dcc.Graph(id='heatmap-graph'),
+    ],
+    style={'width': '49%', 'float': 'right', 'display': 'inline-block'}),
 ])
 
+@app.callback(
+    [Output('court-select', 'options'), Output('pca-graph', 'figure')],
+    [Input('justice-select', 'value')]
+)
+def set_court_options(selected_justice):
+    if selected_justice == 'All':
+        all_courts = list(range(len(courts)))
+        fig = animated_2comp(pca_df, all_courts)
+        return [ {'label': ' '.join(courts[c]), 'value': c} for c in all_courts ], fig
+
+    else:   
+        j_courts = []
+        for i, c in enumerate(courts):
+            if selected_justice in c:
+                j_courts.append(i)
+        fig = animated_2comp(pca_df, j_courts)
+        return [ {'label': ' '.join(courts[c]), 'value': c} for c in j_courts ], fig
+
+@app.callback(
+    Output('heatmap-graph', 'figure'),
+    [Input('court-select', 'value')]
+)
+def update_heatmap(selected_court):
+    justices = courts[selected_court]
+    sim_mat = sim_mats[selected_court]
+    fig = sim_heatmap(sim_mat, justices)
+    return fig
 
 
 if __name__ == '__main__':
--- app/app_functions.py
+++ app/app_functions.py
@@ -4,25 +4,47 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 
+def get_justices(courts):
+    justices = []
+    for court in courts:
+        justices += court
+    justices = sorted(list(set(justices)))
+    return justices
+
 # Plotly graph objects
-def animated_2comp(df):
+def animated_2comp(df, select_courts):
     '''
     Returns Plotly 2 component scatter plot figure
     '''
-    fig = px.scatter(df, x='pc1', y='pc2',
-                     animation_frame='court',
-                     animation_group='justice',
-                     text='justice',
-                     title='Justices Along 2 Components (PCA)',
-                     labels={'pc1': 'PC1', 'pc2': 'PC2'},
-                     width=500,
-                     height=500,
-                     range_x=(-0.2, 1.2),
-                     range_y=(-0.2, 1.2),
-                    )
-    fig.update_traces(textposition='top center')
-    fig.layout.updatemenus[0].buttons[0].args[1]["transition"]["duration"] = 2000
-    fig.layout.updatemenus[0].buttons[0].args[1]["frame"]["duration"] = 3000
+    if select_courts == [0]:
+        temp_df = df.loc[df['court'] == 0]
+        fig = px.scatter(temp_df, x='pc1', y='pc2',
+                        text='justice',
+                        title='Justices Along 2 Components (PCA)',
+                        labels={'pc1': 'Principal Component 1', 'pc2': 'Principal Component 2'},
+                        width=500,
+                        height=500,
+                        range_x=(-0.2, 1.2),
+                        range_y=(-0.2, 1.2),
+                        )
+        fig.update_traces(textposition='top center')
+
+    else:
+        temp_df = df.loc[df['court'].isin(select_courts)]
+        fig = px.scatter(temp_df, x='pc1', y='pc2',
+                        animation_frame='court',
+                        animation_group='justice',
+                        text='justice',
+                        title='Justices Along 2 Components (PCA)',
+                        labels={'pc1': 'Principal Component 1', 'pc2': 'Principal Component 2'},
+                        width=600,
+                        height=600,
+                        range_x=(-0.2, 1.2),
+                        range_y=(-0.2, 1.2),
+                        )
+        fig.update_traces(textposition='top center')
+        fig.layout.updatemenus[0].buttons[0].args[1]["transition"]["duration"] = 2000
+        fig.layout.updatemenus[0].buttons[0].args[1]["frame"]["duration"] = 3000
     return fig
 
 def sim_heatmap(sim_mat, justices):
@@ -31,7 +53,7 @@ def sim_heatmap(sim_mat, justices):
     '''
     fig = go.Figure(data=(go.Heatmap(z=sim_mat, x=justices, y=justices, colorscale='RdBu')))
     fig.update_layout(title='Heatmap of Cosine Similarity Between Justices',
-                      height=500,
-                      width=500,
+                      height=600,
+                      width=600,
                      )
     return fig
