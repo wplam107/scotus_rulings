2d71bf511d8539361582ad6e43e531f179c1a5d4
Wayne <w.p.lam107@gmail.com>
2020-07-23 11:52:15 -0400

    large changes

--- eng_functions.py
+++ eng_functions.py
@@ -1,6 +1,10 @@
 import numpy as np
 import pandas as pd
 
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import MinMaxScaler
+
 # Map code string to metric function (binary)
 def string_2_ints(s):    
     # Special case 'X' is no vote
@@ -52,3 +56,86 @@ def string_2_multi(s):
         value == np.nan
     # Return floor of mean value of justices' opinions
     return value
+
+# Helper function to get sorted list of justices
+def justice_terms(df):
+    justices = list(df.index)
+    justices_by_cases = []
+    for justice in justices:
+        first_case = min(df.loc[justice].dropna().index)
+        last_case = max(df.loc[justice].dropna().index)
+        case_range = (first_case, last_case)
+        justices_by_cases.append((justice, case_range))
+    justices_by_cases.sort(key=lambda x: (x[1][0], x[1][1]))
+    return [ justice[0] for justice in justices_by_cases ]
+
+def get_courts(df):
+    '''
+    Returns list of different court compositions
+    '''
+    courts = []
+    justices = justice_terms(df)
+    i = 0
+    j = 9
+    while j <= len(df.index):
+        court = justices[i:j]
+        courts.append(court)
+        i += 1
+        j += 1
+    return courts
+
+def get_pca(court_op, court):
+    '''
+    2-component representation of justices within a particular court (using PCA), returns DataFrame
+    '''
+    pca = PCA(n_components=2)
+    comp = pd.DataFrame(pca.fit_transform(court_op), index=court, columns=['pc1', 'pc2'])
+
+    # Maintain consistent axes based on Ginsburg/Thomas
+    if comp.loc['Ginsburg']['pc1'] >=0:
+        comp['pc1'] = -comp['pc1']
+    if comp.loc['Thomas']['pc2'] >= 0:
+        comp['pc2'] = -comp['pc2']
+    
+    # Scale for consistency
+    scaler = MinMaxScaler()
+    comp = scaler.fit_transform(comp)
+    comp = pd.DataFrame(comp)
+    
+    comp.reset_index(inplace=True)
+    comp.columns = ['justice', 'pc1', 'pc2']
+    comp['justice'] = court
+    return comp
+
+def all_pca_df(df, court_opinions, courts):
+    '''
+    Returns a merged 2-component DataFrame
+    '''
+    pca_dfs = []
+    for n in range(len(court_opinions)):
+        temp_df = get_pca(court_opinions[n], courts[n])
+        temp_df['court'] = n
+        pca_dfs.append(temp_df)
+
+    pca_dfs = pd.concat(pca_dfs).reset_index(drop=True)
+    return pca_dfs
+
+def get_opinions(df, court):
+    '''
+    Returns numpy array of a particular court's opinions dropping columns with NaN values
+    '''
+    return np.array(df.loc[court].dropna(axis=1))
+
+def get_sim(court_op):
+    '''
+    Returns numpy cosine similarity matrix from an array of opinions
+    '''
+    sim_mat = np.zeros((9,9))
+    for i in range(9):
+        for j in range(9):
+            j_a = court_op[i]
+            j_a = j_a.reshape(1, len(j_a))
+            j_b = court_op[j]
+            j_b = j_b.reshape(1, len(j_b))
+            sim_mat[i][j] = np.round(cosine_similarity(j_a, j_b), 4)
+    return sim_mat
--- features.py
+++ features.py
@@ -1,18 +1,33 @@
 import numpy as np
 import pandas as pd
 import pickle
-from eng_functions import string_2_ints, string_2_multi
+from eng_functions import *
 
 if __name__ == '__main__':
     df = pd.read_csv('scotus_rulings.csv', index_col=0)
 
     # Apply data cleaning and feature engineering functions
-    adj_df = pd.DataFrame(np.vectorize(string_2_ints)(df), index=df.index)
     mul_df = pd.DataFrame(np.vectorize(string_2_multi)(df), index=df.index)
 
-    # Write DataFrames to .csv files
-    with open('./app/data/adj_df.p', 'wb') as f:
-        pickle.dump(adj_df, f)
-    with open('./app/data/mul_df.p', 'wb') as f:
-        pickle.dump(mul_df, f)
+    # Get list of different courts
+    all_courts = get_courts(mul_df)
+    all_court_opinions = [ get_opinions(mul_df, court) for court in all_courts ]
+
+    # Get PCA data for all courts
+    pca_df = all_pca_df(mul_df, all_court_opinions, all_courts)
+
+    # Get similarity matrices for all courts
+    sim_matrices = [ get_sim(court) for court in all_court_opinions ]
+
+    # Pickle data
+    with open('./app/data/courts.p', 'wb') as f:
+        pickle.dump(all_courts, f)
+
+    with open('./app/data/pca_df.p', 'wb') as f:
+        pickle.dump(pca_df, f)
+
+    with open('./app/data/sim_mats.p', 'wb') as f:
+        pickle.dump(sim_matrices, f)
+
     print('Files created')
+
